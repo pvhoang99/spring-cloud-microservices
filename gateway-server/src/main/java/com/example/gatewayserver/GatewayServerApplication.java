c9ef6693ad0cc72f8430e33c805862c9545cc6f9
pham viet hoang <hoang.pv@isofh.com>
2022-04-04 20:53:13 +0700

    socket + gatewway

--- gateway-server/src/main/java/com/example/gatewayserver/GatewayServerApplication.java
+++ gateway-server/src/main/java/com/example/gatewayserver/GatewayServerApplication.java
@@ -2,14 +2,10 @@ package com.example.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableHystrix
-@RefreshScope
 public class GatewayServerApplication {
 
   public static void main(String[] args) {
